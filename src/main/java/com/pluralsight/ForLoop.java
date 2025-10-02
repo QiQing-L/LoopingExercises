e152de0b69a9eb69352767f8806cc6046c7a530f
Qi Qing <learn.qiqing@gmail.com>
2025-10-02 11:31:15 -0400

    Added for loop to display text 5 times.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -2,6 +2,9 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) {
+        for(int count = 0; count < 5; count++ ) {
+            System.out.println("I love Java");
+        }
 
     }
 }
