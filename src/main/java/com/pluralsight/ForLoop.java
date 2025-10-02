934173bcc46b6ee73d37abd33130d4a5faae0e78
Qi Qing <learn.qiqing@gmail.com>
2025-10-02 11:36:30 -0400

    add if condition to display "launch" at the end of countdown in for loop.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -4,6 +4,9 @@ public class ForLoop {
     public static void main(String[] args) {
         for(int count = 10; count > 0; count-- ) {
             System.out.println(count);
+            if(count == 1){
+                System.out.println("Launch!");
+            }
         }
 
     }
