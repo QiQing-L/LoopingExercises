1d7f8bff4e85076a4023be609e3ed1d9d02a34c8
Qi Qing <learn.qiqing@gmail.com>
2025-10-02 11:33:46 -0400

    edit for loop to display 10-1.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) {
-        for(int count = 10; count >= 0; count-- ) {
+        for(int count = 10; count > 0; count-- ) {
             System.out.println(count);
         }
 
