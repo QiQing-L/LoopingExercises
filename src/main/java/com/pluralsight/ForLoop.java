a31c8852311b67605f613c852d491e0c2faab891
Qi Qing <learn.qiqing@gmail.com>
2025-10-02 11:33:21 -0400

    Added for loop to display 10-0.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -2,8 +2,8 @@ package com.pluralsight;
 
 public class ForLoop {
     public static void main(String[] args) {
-        for(int count = 0; count < 5; count++ ) {
-            System.out.println("I love Java");
+        for(int count = 10; count >= 0; count-- ) {
+            System.out.println(count);
         }
 
     }
